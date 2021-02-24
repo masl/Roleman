481025e2fa76bf4bb6de0f1f1958fceade5cad56
masl <contact@masel.dev>
2021-02-24 01:22:50 +0100

    🔨 Setup commands

--- src/index.js
+++ src/index.js
@@ -1,10 +1,36 @@
-const { Client } = require('discord.js');
-const bot = new Client();
+const Discord = require('discord.js');
+const fs = require('fs');
 require('localenv');
 require('dotenv').config();
 
-bot.on('ready', () => {
+const client = new Discord.Client();
+client.commands = new Discord.Collection();
+
+const commandFiles = fs.readdirSync('./src/commands').filter(file => file.endsWith('.js'));
+
+for (const file of commandFiles) {
+	const command = require(`./commands/${file}`);
+	client.commands.set(command.name, command);
+}
+
+client.once('ready', () => {
     console.log('Bot is ready!');
 });
 
-bot.login(process.env.BOT_TOKEN);
+const prefix = '!';
+client.on('message', message => {
+	if (!message.content.startsWith(prefix) || message.author.bot) return;
+
+	const args = message.content.slice(prefix.length).trim().split(/ +/);
+	const commandName = args.shift().toLowerCase();
+	const command = client.commands.get(commandName) || client.commands.find(cmd => cmd.aliases && cmd.aliases.includes(commadnName));
+
+	if (!command) return;
+	try {
+		command.execute(message, client, args);
+	} catch (error) {
+		console.error(error);
+	}
+});
+
+client.login(process.env.BOT_TOKEN);
